b65715371460878f2bfd5dfd1cdd1880ede766a6
Tran Le Hai Duong <duong.tlh.909@aptechlearning.edu.vn>
2022-06-09 16:30:37 +0700

    apply IntersectionObserver instead of scroll window event

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     const aboutImage = document.querySelector('.about-me__image')
     const skill = document.querySelector('.my-skill')
     const navList = document.querySelector('.nav__list')
+    const home = document.querySelector('#home')
+    const header = document.querySelector('.header')
 
     navList.addEventListener('click', function (e) {
         e.preventDefault()
@@ -13,7 +15,7 @@ $(document).ready(function () {
         }
     })
 
-    var typed = new Typed('#intro-span', {
+    new Typed('#intro-span', {
         strings: ['hard coder', 'programmer', 'developer'],
         typeSpeed: 100,
         backSpeed: 60,
@@ -22,32 +24,62 @@ $(document).ready(function () {
 
     var offset_nav = $('.header').offset().top;
     if (offset_nav !== 0) {
-        $('.header').addClass('header-scroll')
+        header.classList.add('header-scroll')
     } else {
-        $('.header').removeClass('header-scroll')
+        header.classList.remove('header-scroll')
     }
-    var avatar_offset = $('.about-me__image').offset().top + aboutImage.height / 3 * 2;
-    var skill_offet = $('.my-skill').offset().top - skill.clientHeight;
-    $(window).scroll(function (event) {
-        var st = $(this).scrollTop();
-        if (st === 0) {
-            $('.header').removeClass('header-scroll')
-        } else {
-            $('.header').addClass('header-scroll')
-        }
-        if (st + window.innerHeight < avatar_offset) {
+
+    const headerCb = (entries) => {
+        const [entry] = entries
+        if (!entry.isIntersecting) {
+            header.classList.add('header-scroll')
+        } else header.classList.remove('header-scroll')
+    }
+
+    const headerObserver = new IntersectionObserver(
+        headerCb, {
+        root: null,
+        threshold: 0.99
+    }
+    )
+
+    headerObserver.observe(home)
+    const aboutImageWidth = aboutImage.getBoundingClientRect().height + 50
+    const avatarCb = (entries) => {
+        const [entry] = entries
+        if (!entry.isIntersecting && entry.boundingClientRect.top > 0) {
             $('.about-me__image').addClass('transform-image')
         } else $('.about-me__image').removeClass('transform-image')
+    }
 
-        if (st < skill_offet) {
+    const avatarObserver = new IntersectionObserver(
+        avatarCb, {
+        root: null,
+        threshold: 0.5,
+        rootMargin: `0px 0px 0px ${aboutImageWidth}px`
+    }
+    )
+    avatarObserver.observe(aboutImage)
+
+    const skillCb = (entries) => {
+        const [entry] = entries
+        if (!entry.isIntersecting) {
             $('.progress-line').removeClass('progress-line-animation');
             $('.progess-line-span').removeClass('progess-line-span-animation')
         } else {
             $('.progress-line').addClass('progress-line-animation');
             $('.progess-line-span').addClass('progess-line-span-animation')
         }
+    }
+    const skillObserver = new IntersectionObserver(
+        skillCb, {
+        root: null,
+        threshold: 0.25
+    }
+    )
 
-    })
+    skillObserver.observe(skill)
+
 
     const showSidebar = () => {
         menuControl.classList.remove('fa-bars')
